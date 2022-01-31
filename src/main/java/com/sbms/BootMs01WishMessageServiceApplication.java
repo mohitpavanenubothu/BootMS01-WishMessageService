4a9740cc2f34d6a8459d9f25f100f18f150736bf
SatyaWork <satyanarayana.e@allvy.com>
2022-01-31 18:13:27 +0530

    Stater file changed

--- src/main/java/com/sbms/BootMs01WishMessageServiceApplication.java
+++ src/main/java/com/sbms/BootMs01WishMessageServiceApplication.java
@@ -10,8 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 public class BootMs01WishMessageServiceApplication {
 
 	public static void main(String[] args) {
-		log.info("Inside stater class for Jenkins test");
+		log.info("Spring Boot Jenkins test 1");
 		SpringApplication.run(BootMs01WishMessageServiceApplication.class, args);
+		log.info("Spring Boot Jenkins test 2");
+		log.info("Spring Boot Jenkins test 3");
 	}
 
 }
